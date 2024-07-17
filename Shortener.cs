85937b26fba0fafb92d69b5f9c4c8e9ca06347cc
Phan Ton Duc Thang <phantonducthang@hotmail.com>
2024-07-17 09:58:56 +0700

    Now It's a true Dumb Calc

---
+++ Shortener.cs
@@ -0,0 +1,7 @@
+public class  Ask
+{
+    public string Addition = "1 is for addition (+)";
+    public string Subtraction = "2 is for Subtraction (-)";
+    public string Multiplication = "3 is for multiplication (x)";
+    public string Division = "4 is for division (:)";
+}
